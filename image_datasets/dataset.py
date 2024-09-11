2c86b2d4ae0e7930f12c071d446bd08dfe6996a5
gradient-diffusion <arseniy555556@gmail.com>
2024-09-11 12:29:16 +0300

    add new features to dataset

--- image_datasets/dataset.py
+++ image_datasets/dataset.py
@@ -7,6 +7,16 @@ from torch.utils.data import Dataset, DataLoader
 import json
 import random
 
+def image_resize(img, max_size=512):
+    w, h = img.size
+    if w >= h:
+        new_w = max_size
+        new_h = int((max_size / w) * h)
+    else:
+        new_h = max_size
+        new_w = int((max_size / h) * w)
+    return img.resize((new_w, new_h))
+
 def c_crop(image):
     width, height = image.size
     new_size = min(width, height)
@@ -17,23 +27,30 @@ def c_crop(image):
     return image.crop((left, top, right, bottom))
 
 class CustomImageDataset(Dataset):
-    def __init__(self, img_dir, img_size=512):
+    def __init__(self, img_dir, img_size=512, caption_type='json'):
         self.images = [os.path.join(img_dir, i) for i in os.listdir(img_dir) if '.jpg' in i or '.png' in i]
         self.images.sort()
         self.img_size = img_size
+        self.caption_type = caption_type
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
         try:
-            img = Image.open(self.images[idx])
-            img = c_crop(img)
-            img = img.resize((self.img_size, self.img_size))
+            img = Image.open(self.images[idx]).convert('RGB')
+            img = image_resize(img, self.img_size)
+            w, h = img.size
+            new_w = (w // 32) * 32
+            new_h = (h // 32) * 32
+            img = img.resize((new_w, new_h))
             img = torch.from_numpy((np.array(img) / 127.5) - 1)
             img = img.permute(2, 0, 1)
-            json_path = self.images[idx].split('.')[0] + '.json'
-            prompt = json.load(open(json_path))['caption']
+            json_path = self.images[idx].split('.')[0] + '.' + self.caption_type
+            if self.caption_type == "json":
+                prompt = json.load(open(json_path))['caption']
+            else:
+                prompt = open(json_path).read()
             return img, prompt
         except Exception as e:
             print(e)
